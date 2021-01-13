1a55748370d48cdd58efb02c376937975dbed30e
arnoson <hallo@arno-schlipf.info>
2021-01-13 18:27:31 +0100

    Get ready for vite 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Plugin } from 'vite'
-import chokidar, { WatchOptions} from 'chokidar'
+import { Plugin, ViteDevServer } from 'vite'
+import chokidar, { WatchOptions } from 'chokidar'
 import chalk from 'chalk'
 
 /** Plugin configuration */
@@ -17,21 +17,18 @@ export default (
   paths: string | readonly string[],
   config: Config = {}
 ): Plugin => ({
-  configureServer: [
-    ({ watcher, config: { root } }) => {
-      const reload = (path: string, action: string) => {
-        // We utilize the `send()` method of the `watcher` instance. I'm not
-        // sure if this is the correct way to send messages to the client but it
-        // works great.
-        watcher.send({ type: 'full-reload', path })
-        if (config.log ?? true) {
-          console.log(chalk.green(`[vite-live-reload] `) + `${path} ${action}.`)
-        }
+  name: 'vite-plugin-live-reload',
+
+  configureServer({ ws, config: { root } }: ViteDevServer) {
+    const reload = (path: string, action: string) => {
+      ws.send({ type: 'full-reload', path })
+      if (config.log ?? true) {
+        console.log(chalk.green(`[vite-live-reload] `) + `${path} ${action}.`)
       }
-      chokidar
-        .watch(paths, { cwd: root, ignoreInitial: true, ...config })
-        .on('add', path => reload(path, 'added'))
-        .on('change', path => reload(path, 'changed'))
     }
-  ]
+    chokidar
+      .watch(paths, { cwd: root, ignoreInitial: true, ...config })
+      .on('add', path => reload(path, 'added'))
+      .on('change', path => reload(path, 'changed'))
+    }
 })
